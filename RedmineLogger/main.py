9a7466c37fc40e6bbf69c6c84eb5db3c12b2b4f8
Dávid Horváth <david.horvath@frontendart.com>
2024-12-19 11:22:39 +0100

    Adding disabled search engine choice screen and option to log holidays

--- RedmineLogger/main.py
+++ RedmineLogger/main.py
@@ -1,5 +1,6 @@
 from getpass import getpass
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
@@ -7,9 +8,12 @@ from selenium.webdriver.support.select import Select
 import time
 import datetime as dt
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--disable-search-engine-choice-screen")
+driver = webdriver.Chrome(chrome_options)
 redmine_login_url = ''
-redmine_new_entry_url = ''
+redmine_new_working_day_entry_url = ''
+redmine_new_holiday_entry_url = ''
 
 
 def start():
@@ -25,10 +29,15 @@ def start():
 
     print("So you want to log {} days started from {} to {}?".format(duration, start_date, end_date))
 
+    logged_days_type = input('Log working days (w) or holidays (h)? ')
+
     confirmation = input('Are you sure (y/n)? ')
 
     if confirmation == 'y':
-        log(start_date, duration)
+        if logged_days_type == 'w':
+            log_working_days(start_date, duration)
+        else:
+            log_holidays(start_date, duration)
     else:
         print('Exiting')
         SystemExit()
@@ -55,10 +64,10 @@ def login():
     login_but.send_keys(Keys.RETURN)
 
 
-def log(start_date, duration):
+def log_working_days(start_date, duration):
     for i in range(int(duration)):
 
-        driver.get(redmine_new_entry_url)
+        driver.get(redmine_new_working_day_entry_url)
         entered_date = start_date + dt.timedelta(days=i)
 
         if entered_date.weekday() == 5 or entered_date.weekday() == 6:
@@ -82,6 +91,45 @@ def log(start_date, duration):
 
             Select(driver.find_element(By.XPATH, '//*[@id="time_entry_activity_id"]')).select_by_visible_text(
                 'Development')
+
+            driver.find_element(By.XPATH, '//*[@id="new_time_entry"]/input[3]').send_keys(Keys.ENTER)
+
+            print('Completed logging day {}/{}'.format(i + 1, duration))
+
+            time.sleep(2)
+
+        print('---------------------------------------')
+
+    print('Logging time completed')
+
+
+def log_holidays(start_date, duration):
+    for i in range(int(duration)):
+
+        driver.get(redmine_new_holiday_entry_url)
+        entered_date = start_date + dt.timedelta(days=i)
+
+        if entered_date.weekday() == 5 or entered_date.weekday() == 6:
+
+            print('{} is weekend, Pass'.format(entered_date))
+
+        else:
+
+            print('Entered date -> {}'.format(entered_date))
+
+            date_box = driver.find_element(By.XPATH, '//*[@id="time_entry_spent_on"]')
+            date_box.clear()
+            date_box.send_keys(str(entered_date.strftime('%Y')))
+            date_box.send_keys(Keys.TAB)
+            date_box.send_keys(str(entered_date.strftime('%m')))
+            date_box.send_keys(str(entered_date.strftime('%d')))
+
+            driver.find_element(By.XPATH, '//*[@id="time_entry_hours"]').send_keys('8')
+
+            driver.find_element(By.XPATH, '//*[@id="time_entry_comments"]').send_keys('szabadság')
+
+            Select(driver.find_element(By.XPATH, '//*[@id="time_entry_activity_id"]')).select_by_visible_text(
+                'Administration')
 
             driver.find_element(By.XPATH, '//*[@id="new_time_entry"]/input[3]').send_keys(Keys.ENTER)
 
